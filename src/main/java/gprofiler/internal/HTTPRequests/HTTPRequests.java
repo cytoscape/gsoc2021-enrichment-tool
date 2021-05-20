13a4c3a28ac906dbf2db2598fa6fd8c0b403ae48
ighosh <ighosh98>
2021-05-20 15:39:34 +0530

    Initial setup commit

---
+++ src/main/java/gprofiler/internal/HTTPRequests/HTTPRequests.java
@@ -0,0 +1,11 @@
+package gprofiler.internal.HTTPRequests;
+
+
+/**
+ * For handling API requests to gProfiler
+ */
+public class HTTPRequests {
+    private final String USER_AGENT = "Mozilla/5.0";
+    private final String basicURL = "https://biit.cs.ut.ee/gprofiler/api/";
+
+};
