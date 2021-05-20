daec1e5aaabf2360fc8f9b9e2241c77ad7124d18
ighosh <ighosh98>
2021-05-20 16:11:42 +0530

    Add template to handle get and post requests

--- src/main/java/gprofiler/internal/HTTPRequests/HTTPRequests.java
+++ src/main/java/gprofiler/internal/HTTPRequests/HTTPRequests.java
@@ -1,5 +1,14 @@
 package gprofiler.internal.HTTPRequests;
-
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpClient;
+import java.util.HashMap;
+import java.util.Map;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import java.lang.reflect.Type;
 
 /**
  * For handling API requests to gProfiler
@@ -7,5 +16,36 @@ package gprofiler.internal.HTTPRequests;
 public class HTTPRequests {
     private final String USER_AGENT = "Mozilla/5.0";
     private final String basicURL = "https://biit.cs.ut.ee/gprofiler/api/";
-
+    public HttpResponse<String> makePostRequest(String endpoint , Map<String,String> parameters) throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        StringBuffer urlConverter = new StringBuffer();
+        urlConverter.append(this.basicURL);
+        urlConverter.append(endpoint);
+        String url = urlConverter.toString();
+        Gson gson = new Gson();
+        Type gsonType = new TypeToken<HashMap>(){}.getType();
+        String jsonBody = gson.toJson(parameters,gsonType);
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .header("Content-Type","application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(jsonBody))
+                .build();
+        HttpResponse<String> response = client.send(request,HttpResponse.BodyHandlers.ofString());
+        return response;
+    }
+    public HttpResponse<String> makeGetRequests(String endpoint) throws IOException,InterruptedException {
+        //fetches data using a specific api endpoint
+        HttpClient client = HttpClient.newHttpClient();
+        StringBuffer urlConverter = new StringBuffer();
+        urlConverter.append(this.basicURL);
+        urlConverter.append(endpoint);
+        String url = urlConverter.toString();
+        HttpRequest request = HttpRequest.newBuilder()
+                .GET()
+                .header("accept","application/json")
+                .uri(URI.create(url))
+                .build();
+        HttpResponse<String> response = client.send(request,HttpResponse.BodyHandlers.ofString());
+        return response;
+    }
 };
